2302307d59b93c42a7d7d7df3e67d20f12cc2e92
Wu Di Ao <406714601@qq.com>
2017-05-31 20:45:19 +0800

    gender+birthday

--- app/src/main/java/com/example/corodinatelayout/UserSettingActicity.java
+++ app/src/main/java/com/example/corodinatelayout/UserSettingActicity.java
@@ -1,15 +1,22 @@
 package com.example.corodinatelayout;
-
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+
+import com.alertdialogpro.AlertDialogPro;
 
 /**
  * Created by wda on 2017/5/24.
  */
 public class UserSettingActicity extends AppCompatActivity {
+    private EditText GendereditText;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,18 @@ public class UserSettingActicity extends AppCompatActivity {
         toolbar.setTitle("个人资料");
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorTitle));
         toolbar.inflateMenu(R.menu.user_menu);
-
+        GendereditText = (EditText)findViewById(R.id.gender);
+        GendereditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialogPro.Builder builder = new AlertDialogPro.Builder(getApplicationContext());
+                builder.setTitle("性别").
+                        setMessage("Message Body").
+                        setNeutralButton("Neutral", null).
+                        setPositiveButton("Positive", null).
+                        setNegativeButton("Negative", null).
+                        show();
+            }
+        });
     }
 }
