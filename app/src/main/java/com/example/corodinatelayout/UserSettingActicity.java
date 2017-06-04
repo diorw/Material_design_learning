7818c4e7a57c68bd5fc4fae015b1657895621de8
Wu Di Ao <406714601@qq.com>
2017-06-04 13:48:38 +0800

    delete sth

--- app/src/main/java/com/example/corodinatelayout/UserSettingActicity.java
+++ app/src/main/java/com/example/corodinatelayout/UserSettingActicity.java
@@ -1,7 +1,5 @@
 package com.example.corodinatelayout;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +8,6 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 
-import com.alertdialogpro.AlertDialogPro;
 
 /**
  * Created by wda on 2017/5/24.
@@ -28,18 +25,5 @@ public class UserSettingActicity extends AppCompatActivity {
         toolbar.setTitle("个人资料");
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorTitle));
         toolbar.inflateMenu(R.menu.user_menu);
-        GendereditText = (EditText)findViewById(R.id.gender);
-        GendereditText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialogPro.Builder builder = new AlertDialogPro.Builder(getApplicationContext());
-                builder.setTitle("性别").
-                        setMessage("Message Body").
-                        setNeutralButton("Neutral", null).
-                        setPositiveButton("Positive", null).
-                        setNegativeButton("Negative", null).
-                        show();
-            }
-        });
     }
 }
